9b2b1f72f97882f20ce7372214638ed8934463c3
DANIELLE ALBA <danib.sgh@gmail.com>
2019-11-20 18:45:19 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+superbowlWin = (arr) =>{
+  const answer = arr.find(e => e.result == "W")
+   if(answer == undefined ){
+     return undefined 
+   }
+  else return answer.year
+}
